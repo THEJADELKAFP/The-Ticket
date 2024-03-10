20970c019dfcfa7954a70c6c6db00432e7ee1e41
MinniFinnex <136409751+MinniFinnex@users.noreply.github.com>
2024-03-10 14:33:20 -0600

    blur menu

--- my_flask_project/app.py
+++ my_flask_project/app.py
@@ -4,8 +4,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def index():
+    title = "Inicio"
+    return render_template("index.html", title=title)
+
+@app.route("/contacto")
+def contacto():
+    title = "Contacto"
+    return render_template("Contacto.html", title=title)
+
+@app.route("/about")
+def about():
+    title = "about"
+    return render_template("about.html", title=title)
 
 if __name__ == "__main__":
   app.run(debug=True, port=3000)
