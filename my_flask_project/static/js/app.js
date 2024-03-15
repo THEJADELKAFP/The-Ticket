2100911a1c0b28cf86e881a8c0b35194c2eb4188
JADELKAFFP <144500434+THEJADELKAFP@users.noreply.github.com>
2024-03-15 11:38:30 -0500

    mas contenido about

--- my_flask_project/static/js/app.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector('body');
-  /* A medida que se desplaza la página, aumenta el desenfoque */
-  $(window).scroll(function(e) {
-    var distanceScrolled = $(this).scrollTop();
-    $('.menu').css('-webkit-filter', 'blur(' + distanceScrolled / 60 + 'px)');
-  });
-  
